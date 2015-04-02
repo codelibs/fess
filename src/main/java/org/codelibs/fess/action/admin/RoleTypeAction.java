5da3fc1596a77d73d742c7cacf88612972f6d48b
null44 <greenstar77@gmail.com>
2015-04-02 17:14:25 +0900

    merge RoleTypeAction and BsRoleTypeAction

--- src/main/java/org/codelibs/fess/action/admin/RoleTypeAction.java
+++ src/main/java/org/codelibs/fess/action/admin/RoleTypeAction.java
@@ -17,36 +17,58 @@
 package org.codelibs.fess.action.admin;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.codelibs.fess.action.base.FessAdminAction;
 import org.codelibs.fess.beans.FessBeans;
 import org.codelibs.fess.crud.CommonConstants;
 import org.codelibs.fess.crud.CrudMessageException;
-import org.codelibs.fess.crud.action.admin.BsRoleTypeAction;
 import org.codelibs.fess.crud.util.SAStrutsUtil;
 import org.codelibs.fess.db.exentity.RoleType;
+import org.codelibs.fess.form.admin.RoleTypeForm;
 import org.codelibs.fess.helper.SystemHelper;
+import org.codelibs.fess.pager.RoleTypePager;
+import org.codelibs.fess.service.RoleTypeService;
 import org.codelibs.sastruts.core.annotation.Token;
 import org.codelibs.sastruts.core.exception.SSCActionMessagesException;
+import org.seasar.framework.beans.util.Beans;
+import org.seasar.struts.annotation.ActionForm;
 import org.seasar.struts.annotation.Execute;
 import org.seasar.struts.exception.ActionMessagesException;
+import org.seasar.framework.util.StringUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class RoleTypeAction extends BsRoleTypeAction {
-    private static final long serialVersionUID = 1L;
-
-    private static final Log log = LogFactory.getLog(RoleTypeAction.class);
+public class RoleTypeAction extends FessAdminAction {
+    private static final Logger logger = LoggerFactory.getLogger(RoleTypeAction.class);
 
     @Resource
     protected SystemHelper systemHelper;
+
+    // for list
+
+    public List<RoleType> roleTypeItems;
+
+    // for edit/confirm/delete
+
+    @ActionForm
+    @Resource
+    protected RoleTypeForm roleTypeForm;
+
+    @Resource
+    protected RoleTypeService roleTypeService;
+
+    @Resource
+    protected RoleTypePager roleTypePager;
 
     public String getHelpLink() {
         return systemHelper.getHelpLink("roleType");
     }
 
-    @Override
     @Token(save = false, validate = true, keep = true)
     @Execute(validator = true, input = "edit.jsp")
     public String confirmfromcreate() {
@@ -54,7 +76,6 @@ public class RoleTypeAction extends BsRoleTypeAction {
         return "confirm.jsp";
     }
 
-    @Override
     @Token(save = false, validate = true, keep = true)
     @Execute(validator = true, input = "edit.jsp")
     public String confirmfromupdate() {
@@ -62,9 +83,164 @@ public class RoleTypeAction extends BsRoleTypeAction {
         return "confirm.jsp";
     }
 
-    @Override
-    protected void loadRoleType() {
+    @Execute(validator = false, input = "error.jsp")
+    public String index() {
+        return displayList(false);
+    }
 
+    @Execute(validator = false, input = "error.jsp", urlPattern = "list/{pageNumber}")
+    public String list() {
+        // page navi
+        if (StringUtil.isNotBlank(roleTypeForm.pageNumber)) {
+            try {
+                roleTypePager.setCurrentPageNumber(Integer.parseInt(roleTypeForm.pageNumber));
+            } catch (final NumberFormatException e) {
+                if (logger.isDebugEnabled()) {
+                    logger.debug("Invalid value: " + roleTypeForm.pageNumber, e);
+                }
+            }
+        }
+
+        return displayList(false);
+    }
+
+    @Execute(validator = false, input = "error.jsp")
+    public String search() {
+        Beans.copy(roleTypeForm.searchParams, roleTypePager).excludes(CommonConstants.PAGER_CONVERSION_RULE)
+
+        .execute();
+
+        return displayList(false);
+    }
+
+    @Execute(validator = false, input = "error.jsp")
+    public String reset() {
+        roleTypePager.clear();
+
+        return displayList(false);
+    }
+
+    @Execute(validator = false, input = "error.jsp")
+    public String back() {
+        return displayList(false);
+    }
+
+    @Token(save = true, validate = false)
+    @Execute(validator = false, input = "error.jsp")
+    public String editagain() {
+        return "edit.jsp";
+    }
+
+    @Execute(validator = false, input = "error.jsp", urlPattern = "confirmpage/{crudMode}/{id}")
+    public String confirmpage() {
+        if (roleTypeForm.crudMode != CommonConstants.CONFIRM_MODE) {
+            throw new ActionMessagesException("errors.crud_invalid_mode", new Object[] { CommonConstants.CONFIRM_MODE,
+                    roleTypeForm.crudMode });
+        }
+
+        loadRoleType();
+
+        return "confirm.jsp";
+    }
+
+    @Token(save = true, validate = false)
+    @Execute(validator = false, input = "error.jsp")
+    public String createpage() {
+        // page navi
+        roleTypeForm.initialize();
+        roleTypeForm.crudMode = CommonConstants.CREATE_MODE;
+
+        return "edit.jsp";
+    }
+
+    @Token(save = true, validate = false)
+    @Execute(validator = false, input = "error.jsp", urlPattern = "editpage/{crudMode}/{id}")
+    public String editpage() {
+        if (roleTypeForm.crudMode != CommonConstants.EDIT_MODE) {
+            throw new ActionMessagesException("errors.crud_invalid_mode", new Object[] { CommonConstants.EDIT_MODE, roleTypeForm.crudMode });
+        }
+
+        loadRoleType();
+
+        return "edit.jsp";
+    }
+
+    @Token(save = true, validate = false)
+    @Execute(validator = false, input = "error.jsp")
+    public String editfromconfirm() {
+        roleTypeForm.crudMode = CommonConstants.EDIT_MODE;
+
+        loadRoleType();
+
+        return "edit.jsp";
+    }
+
+    @Token(save = true, validate = false)
+    @Execute(validator = false, input = "error.jsp", urlPattern = "deletepage/{crudMode}/{id}")
+    public String deletepage() {
+        if (roleTypeForm.crudMode != CommonConstants.DELETE_MODE) {
+            throw new ActionMessagesException("errors.crud_invalid_mode",
+                    new Object[] { CommonConstants.DELETE_MODE, roleTypeForm.crudMode });
+        }
+
+        loadRoleType();
+
+        return "confirm.jsp";
+    }
+
+    @Token(save = true, validate = false)
+    @Execute(validator = false, input = "error.jsp")
+    public String deletefromconfirm() {
+        roleTypeForm.crudMode = CommonConstants.DELETE_MODE;
+
+        loadRoleType();
+
+        return "confirm.jsp";
+    }
+
+    @Token(save = false, validate = true)
+    @Execute(validator = true, input = "edit.jsp")
+    public String create() {
+        try {
+            final RoleType roleType = createRoleType();
+            roleTypeService.store(roleType);
+            SAStrutsUtil.addSessionMessage("success.crud_create_crud_table");
+
+            return displayList(true);
+        } catch (final ActionMessagesException e) {
+            logger.error(e.getMessage(), e);
+            throw e;
+        } catch (final CrudMessageException e) {
+            logger.error(e.getMessage(), e);
+            throw new ActionMessagesException(e.getMessageId(), e.getArgs());
+        } catch (final Exception e) {
+            logger.error(e.getMessage(), e);
+            throw new ActionMessagesException("errors.crud_failed_to_create_crud_table");
+        }
+    }
+
+    @Token(save = false, validate = true)
+    @Execute(validator = true, input = "edit.jsp")
+    public String update() {
+        try {
+            final RoleType roleType = createRoleType();
+            roleTypeService.store(roleType);
+            SAStrutsUtil.addSessionMessage("success.crud_update_crud_table");
+
+            return displayList(true);
+        } catch (final ActionMessagesException e) {
+            logger.error(e.getMessage(), e);
+            throw e;
+        } catch (final CrudMessageException e) {
+            logger.error(e.getMessage(), e);
+            throw new ActionMessagesException(e.getMessageId(), e.getArgs());
+        } catch (final Exception e) {
+            logger.error(e.getMessage(), e);
+            throw new ActionMessagesException("errors.crud_failed_to_update_crud_table");
+        }
+    }
+
+    protected void loadRoleType() {
         final RoleType roleType = roleTypeService.getRoleType(createKeyMap());
         if (roleType == null) {
             // throw an exception
@@ -74,7 +250,14 @@ public class RoleTypeAction extends BsRoleTypeAction {
         FessBeans.copy(roleType, roleTypeForm).commonColumnDateConverter().excludes("searchParams", "mode").execute();
     }
 
-    @Override
+    protected Map<String, String> createKeyMap() {
+        final Map<String, String> keys = new HashMap<String, String>();
+
+        keys.put("id", roleTypeForm.id);
+
+        return keys;
+    }
+
     protected RoleType createRoleType() {
         RoleType roleType;
         final String username = systemHelper.getUsername();
@@ -98,7 +281,6 @@ public class RoleTypeAction extends BsRoleTypeAction {
         return roleType;
     }
 
-    @Override
     @Token(save = false, validate = true)
     @Execute(validator = false, input = "error.jsp")
     public String delete() {
@@ -124,14 +306,30 @@ public class RoleTypeAction extends BsRoleTypeAction {
 
             return displayList(true);
         } catch (final ActionMessagesException e) {
-            log.error(e.getMessage(), e);
+            logger.error(e.getMessage(), e);
             throw e;
         } catch (final CrudMessageException e) {
-            log.error(e.getMessage(), e);
+            logger.error(e.getMessage(), e);
             throw new SSCActionMessagesException(e, e.getMessageId(), e.getArgs());
         } catch (final Exception e) {
-            log.error(e.getMessage(), e);
+            logger.error(e.getMessage(), e);
             throw new SSCActionMessagesException(e, "errors.crud_failed_to_delete_crud_table");
+        }
+    }
+
+    protected String displayList(final boolean redirect) {
+        // page navi
+        roleTypeItems = roleTypeService.getRoleTypeList(roleTypePager);
+
+        // restore from pager
+        Beans.copy(roleTypePager, roleTypeForm.searchParams).excludes(CommonConstants.PAGER_CONVERSION_RULE)
+
+        .execute();
+
+        if (redirect) {
+            return "index?redirect=true";
+        } else {
+            return "index.jsp";
         }
     }
 }
