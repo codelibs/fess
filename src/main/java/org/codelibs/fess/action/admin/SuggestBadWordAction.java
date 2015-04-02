2ff2b3d8245c50e2ad38f0bb238cdf4bb7cad1e7
null44 <greenstar77@gmail.com>
2015-04-02 18:39:11 +0900

    merge SuggestBadWordAction and BsSuggestBadWordAction

--- src/main/java/org/codelibs/fess/action/admin/SuggestBadWordAction.java
+++ src/main/java/org/codelibs/fess/action/admin/SuggestBadWordAction.java
@@ -27,36 +27,59 @@ import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Writer;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.codelibs.core.util.DynamicProperties;
 import org.codelibs.fess.Constants;
 import org.codelibs.fess.FessSystemException;
+import org.codelibs.fess.action.base.FessAdminAction;
 import org.codelibs.fess.beans.FessBeans;
 import org.codelibs.fess.crud.CommonConstants;
 import org.codelibs.fess.crud.CrudMessageException;
-import org.codelibs.fess.crud.action.admin.BsSuggestBadWordAction;
 import org.codelibs.fess.crud.util.SAStrutsUtil;
 import org.codelibs.fess.db.exentity.SuggestBadWord;
+import org.codelibs.fess.form.admin.SuggestBadWordForm;
 import org.codelibs.fess.helper.SuggestHelper;
 import org.codelibs.fess.helper.SystemHelper;
+import org.codelibs.fess.pager.SuggestBadWordPager;
+import org.codelibs.fess.service.SuggestBadWordService;
 import org.codelibs.robot.util.StreamUtil;
 import org.codelibs.sastruts.core.annotation.Token;
 import org.codelibs.sastruts.core.exception.SSCActionMessagesException;
+import org.seasar.framework.beans.util.Beans;
+import org.seasar.framework.util.StringUtil;
+import org.seasar.struts.annotation.ActionForm;
 import org.seasar.struts.annotation.Execute;
 import org.seasar.struts.exception.ActionMessagesException;
 import org.seasar.struts.util.ResponseUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class SuggestBadWordAction extends BsSuggestBadWordAction {
+public class SuggestBadWordAction extends FessAdminAction {
 
-    private static final long serialVersionUID = 1L;
+    private static final Logger logger = LoggerFactory.getLogger(SuggestBadWordAction.class);
 
-    private static final Log log = LogFactory.getLog(SuggestBadWordAction.class);
+    // for list
+
+    public List<SuggestBadWord> suggestBadWordItems;
+
+    // for edit/confirm/delete
+
+    @ActionForm
+    @Resource
+    protected SuggestBadWordForm suggestBadWordForm;
+
+    @Resource
+    protected SuggestBadWordService suggestBadWordService;
+
+    @Resource
+    protected SuggestBadWordPager suggestBadWordPager;
 
     @Resource
     protected SystemHelper systemHelper;
@@ -67,11 +90,162 @@ public class SuggestBadWordAction extends BsSuggestBadWordAction {
     @Resource
     protected DynamicProperties crawlerProperties;
 
+    protected String displayList(final boolean redirect) {
+        // page navi
+        suggestBadWordItems = suggestBadWordService.getSuggestBadWordList(suggestBadWordPager);
+
+        // restore from pager
+        Beans.copy(suggestBadWordPager, suggestBadWordForm.searchParams).excludes(CommonConstants.PAGER_CONVERSION_RULE)
+
+        .execute();
+
+        if (redirect) {
+            return "index?redirect=true";
+        } else {
+            return "index.jsp";
+        }
+    }
+
+    @Execute(validator = false, input = "error.jsp")
+    public String index() {
+        return displayList(false);
+    }
+
+    @Execute(validator = false, input = "error.jsp", urlPattern = "list/{pageNumber}")
+    public String list() {
+        // page navi
+        if (StringUtil.isNotBlank(suggestBadWordForm.pageNumber)) {
+            try {
+                suggestBadWordPager.setCurrentPageNumber(Integer.parseInt(suggestBadWordForm.pageNumber));
+            } catch (final NumberFormatException e) {
+                if (logger.isDebugEnabled()) {
+                    logger.debug("Invalid value: " + suggestBadWordForm.pageNumber, e);
+                }
+            }
+        }
+
+        return displayList(false);
+    }
+
+    @Execute(validator = false, input = "error.jsp")
+    public String search() {
+        Beans.copy(suggestBadWordForm.searchParams, suggestBadWordPager).excludes(CommonConstants.PAGER_CONVERSION_RULE)
+
+        .execute();
+
+        return displayList(false);
+    }
+
+    @Execute(validator = false, input = "error.jsp")
+    public String reset() {
+        suggestBadWordPager.clear();
+
+        return displayList(false);
+    }
+
+    @Execute(validator = false, input = "error.jsp")
+    public String back() {
+        return displayList(false);
+    }
+
+    @Token(save = true, validate = false)
+    @Execute(validator = false, input = "error.jsp")
+    public String editagain() {
+        return "edit.jsp";
+    }
+
+    @Execute(validator = false, input = "error.jsp", urlPattern = "confirmpage/{crudMode}/{id}")
+    public String confirmpage() {
+        if (suggestBadWordForm.crudMode != CommonConstants.CONFIRM_MODE) {
+            throw new ActionMessagesException("errors.crud_invalid_mode", new Object[] { CommonConstants.CONFIRM_MODE,
+                    suggestBadWordForm.crudMode });
+        }
+
+        loadSuggestBadWord();
+
+        return "confirm.jsp";
+    }
+
+    @Token(save = true, validate = false)
+    @Execute(validator = false, input = "error.jsp")
+    public String createpage() {
+        // page navi
+        suggestBadWordForm.initialize();
+        suggestBadWordForm.crudMode = CommonConstants.CREATE_MODE;
+
+        return "edit.jsp";
+    }
+
+    @Token(save = true, validate = false)
+    @Execute(validator = false, input = "error.jsp", urlPattern = "editpage/{crudMode}/{id}")
+    public String editpage() {
+        if (suggestBadWordForm.crudMode != CommonConstants.EDIT_MODE) {
+            throw new ActionMessagesException("errors.crud_invalid_mode", new Object[] { CommonConstants.EDIT_MODE,
+                    suggestBadWordForm.crudMode });
+        }
+
+        loadSuggestBadWord();
+
+        return "edit.jsp";
+    }
+
+    @Token(save = true, validate = false)
+    @Execute(validator = false, input = "error.jsp")
+    public String editfromconfirm() {
+        suggestBadWordForm.crudMode = CommonConstants.EDIT_MODE;
+
+        loadSuggestBadWord();
+
+        return "edit.jsp";
+    }
+
+    @Token(save = false, validate = true, keep = true)
+    @Execute(validator = true, input = "edit.jsp")
+    public String confirmfromcreate() {
+        return "confirm.jsp";
+    }
+
+    @Token(save = false, validate = true, keep = true)
+    @Execute(validator = true, input = "edit.jsp")
+    public String confirmfromupdate() {
+        return "confirm.jsp";
+    }
+
+    @Token(save = true, validate = false)
+    @Execute(validator = false, input = "error.jsp", urlPattern = "deletepage/{crudMode}/{id}")
+    public String deletepage() {
+        if (suggestBadWordForm.crudMode != CommonConstants.DELETE_MODE) {
+            throw new ActionMessagesException("errors.crud_invalid_mode", new Object[] { CommonConstants.DELETE_MODE,
+                    suggestBadWordForm.crudMode });
+        }
+
+        loadSuggestBadWord();
+
+        return "confirm.jsp";
+    }
+
+    @Token(save = true, validate = false)
+    @Execute(validator = false, input = "error.jsp")
+    public String deletefromconfirm() {
+        suggestBadWordForm.crudMode = CommonConstants.DELETE_MODE;
+
+        loadSuggestBadWord();
+
+        return "confirm.jsp";
+    }
+
+    protected Map<String, String> createKeyMap() {
+        final Map<String, String> keys = new HashMap<String, String>();
+
+        keys.put("id", suggestBadWordForm.id);
+
+        return keys;
+    }
+
     public String getHelpLink() {
         return systemHelper.getHelpLink("suggestBadWord");
     }
 
-    @Override
     protected void loadSuggestBadWord() {
 
         final SuggestBadWord suggestBadWord = suggestBadWordService.getSuggestBadWord(createKeyMap());
@@ -83,7 +257,6 @@ public class SuggestBadWordAction extends BsSuggestBadWordAction {
         FessBeans.copy(suggestBadWord, suggestBadWordForm).commonColumnDateConverter().excludes("searchParams", "mode").execute();
     }
 
-    @Override
     protected SuggestBadWord createSuggestBadWord() {
         SuggestBadWord suggestBadWord;
         final String username = systemHelper.getUsername();
@@ -106,7 +279,6 @@ public class SuggestBadWordAction extends BsSuggestBadWordAction {
         return suggestBadWord;
     }
 
-    @Override
     @Execute(validator = false, input = "error.jsp")
     public String delete() {
         if (suggestBadWordForm.crudMode != CommonConstants.DELETE_MODE) {
@@ -133,18 +305,17 @@ public class SuggestBadWordAction extends BsSuggestBadWordAction {
 
             return displayList(true);
         } catch (final ActionMessagesException e) {
-            log.error(e.getMessage(), e);
+            logger.error(e.getMessage(), e);
             throw e;
         } catch (final CrudMessageException e) {
-            log.error(e.getMessage(), e);
+            logger.error(e.getMessage(), e);
             throw new SSCActionMessagesException(e, e.getMessageId(), e.getArgs());
         } catch (final Exception e) {
-            log.error(e.getMessage(), e);
+            logger.error(e.getMessage(), e);
             throw new SSCActionMessagesException(e, "errors.crud_failed_to_delete_crud_table");
         }
     }
 
-    @Override
     @Token(save = false, validate = true)
     @Execute(validator = true, input = "edit.jsp")
     public String create() {
@@ -157,18 +328,17 @@ public class SuggestBadWordAction extends BsSuggestBadWordAction {
 
             return displayList(true);
         } catch (final ActionMessagesException e) {
-            log.error(e.getMessage(), e);
+            logger.error(e.getMessage(), e);
             throw e;
         } catch (final CrudMessageException e) {
-            log.error(e.getMessage(), e);
+            logger.error(e.getMessage(), e);
             throw new ActionMessagesException(e.getMessageId(), e.getArgs());
         } catch (final Exception e) {
-            log.error(e.getMessage(), e);
+            logger.error(e.getMessage(), e);
             throw new ActionMessagesException("errors.crud_failed_to_create_crud_table");
         }
     }
 
-    @Override
     @Token(save = false, validate = true)
     @Execute(validator = true, input = "edit.jsp")
     public String update() {
@@ -181,13 +351,13 @@ public class SuggestBadWordAction extends BsSuggestBadWordAction {
 
             return displayList(true);
         } catch (final ActionMessagesException e) {
-            log.error(e.getMessage(), e);
+            logger.error(e.getMessage(), e);
             throw e;
         } catch (final CrudMessageException e) {
-            log.error(e.getMessage(), e);
+            logger.error(e.getMessage(), e);
             throw new ActionMessagesException(e.getMessageId(), e.getArgs());
         } catch (final Exception e) {
-            log.error(e.getMessage(), e);
+            logger.error(e.getMessage(), e);
             throw new ActionMessagesException("errors.crud_failed_to_update_crud_table");
         }
     }
@@ -211,7 +381,7 @@ public class SuggestBadWordAction extends BsSuggestBadWordAction {
                         Constants.CSV_FILE_ENCODING_PROPERTY, Constants.UTF_8)))) {
             suggestBadWordService.exportCsv(writer);
         } catch (final Exception e) {
-            log.error("Failed to export data.", e);
+            logger.error("Failed to export data.", e);
             throw new SSCActionMessagesException(e, "errors.failed_to_export_data");
         }
         return null;
@@ -242,9 +412,9 @@ public class SuggestBadWordAction extends BsSuggestBadWordAction {
             StreamUtil.drain(is, fos);
         } catch (final Exception e) {
             if (tempFile != null && !tempFile.delete()) {
-                log.warn("Could not delete " + tempFile.getAbsolutePath());
+                logger.warn("Could not delete " + tempFile.getAbsolutePath());
             }
-            log.error("Failed to import data.", e);
+            logger.error("Failed to import data.", e);
             throw new SSCActionMessagesException(e, "errors.failed_to_import_data");
         } finally {
             IOUtils.closeQuietly(is);
@@ -255,23 +425,22 @@ public class SuggestBadWordAction extends BsSuggestBadWordAction {
         try {
             final String head = new String(b, Constants.UTF_8);
             if (!(head.startsWith("\"BadWord\"") || head.startsWith("BadWord"))) {
-                log.error("Unknown file: " + suggestBadWordForm.suggestBadWordFile);
+                logger.error("Unknown file: " + suggestBadWordForm.suggestBadWordFile);
                 throw new SSCActionMessagesException("errors.unknown_import_file");
             }
             final String enc = crawlerProperties.getProperty(Constants.CSV_FILE_ENCODING_PROPERTY, Constants.UTF_8);
             new Thread(new Runnable() {
-                @Override
                 public void run() {
                     Reader reader = null;
                     try {
                         reader = new BufferedReader(new InputStreamReader(new FileInputStream(oFile), enc));
                         suggestBadWordService.importCsv(reader);
                     } catch (final Exception e) {
-                        log.error("Failed to import data.", e);
+                        logger.error("Failed to import data.", e);
                         throw new FessSystemException("Failed to import data.", e);
                     } finally {
                         if (!oFile.delete()) {
-                            log.warn("Could not delete " + oFile.getAbsolutePath());
+                            logger.warn("Could not delete " + oFile.getAbsolutePath());
                         }
                         IOUtils.closeQuietly(reader);
                         suggestHelper.deleteAllBadWord();
@@ -281,14 +450,14 @@ public class SuggestBadWordAction extends BsSuggestBadWordAction {
             }).start();
         } catch (final ActionMessagesException e) {
             if (!oFile.delete()) {
-                log.warn("Could not delete " + oFile.getAbsolutePath());
+                logger.warn("Could not delete " + oFile.getAbsolutePath());
             }
             throw e;
         } catch (final Exception e) {
             if (!oFile.delete()) {
-                log.warn("Could not delete " + oFile.getAbsolutePath());
+                logger.warn("Could not delete " + oFile.getAbsolutePath());
             }
-            log.error("Failed to import data.", e);
+            logger.error("Failed to import data.", e);
             throw new SSCActionMessagesException(e, "errors.failed_to_import_data");
         }
         SAStrutsUtil.addSessionMessage("success.upload_suggest_bad_word");
