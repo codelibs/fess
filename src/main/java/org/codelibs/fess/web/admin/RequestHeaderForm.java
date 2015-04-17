2767d81b11e671f3c70b29413fc03c7bcfcca7db
Shunji Makino <makino@n2sm.net>
2015-04-17 19:26:26 +0900

    RequestHeaderForm.java

--- src/main/java/org/codelibs/fess/web/admin/RequestHeaderForm.java
+++ src/main/java/org/codelibs/fess/web/admin/RequestHeaderForm.java
@@ -19,17 +19,84 @@ package org.codelibs.fess.web.admin;
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.codelibs.fess.Constants;
-import org.codelibs.fess.crud.form.admin.BsRequestHeaderForm;
+import org.seasar.struts.annotation.DateType;
+import org.seasar.struts.annotation.IntegerType;
+import org.seasar.struts.annotation.LongType;
+import org.seasar.struts.annotation.Maxbytelength;
+import org.seasar.struts.annotation.Required;
 
-public class RequestHeaderForm extends BsRequestHeaderForm implements Serializable {
+public class RequestHeaderForm implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @Override
+    @IntegerType
+    public String pageNumber;
+
+    public Map<String, String> searchParams = new HashMap<String, String>();
+
+    @IntegerType
+    public int crudMode;
+
+    public String getCurrentPageNumber() {
+        return pageNumber;
+    }
+
+    @Required(target = "confirmfromupdate,update,delete")
+    @LongType
+    public String id;
+
+    @Required(target = "confirmfromcreate,create,confirmfromupdate,update,delete")
+    @Maxbytelength(maxbytelength = 100)
+    public String name;
+
+    @Required(target = "confirmfromcreate,create,confirmfromupdate,update,delete")
+    @Maxbytelength(maxbytelength = 1000)
+    public String value;
+
+    @Required(target = "confirmfromcreate,create,confirmfromupdate,update,delete")
+    @LongType
+    public String webCrawlingConfigId;
+
+    @Required(target = "confirmfromupdate,update,delete")
+    @Maxbytelength(maxbytelength = 255)
+    public String createdBy;
+
+    @Required(target = "confirmfromupdate,update,delete")
+    @DateType(datePattern = Constants.DEFAULT_DATETIME_FORMAT)
+    public String createdTime;
+
+    @Maxbytelength(maxbytelength = 255)
+    public String updatedBy;
+
+    @DateType(datePattern = Constants.DEFAULT_DATETIME_FORMAT)
+    public String updatedTime;
+
+    @Maxbytelength(maxbytelength = 255)
+    public String deletedBy;
+
+    @DateType(datePattern = Constants.DEFAULT_DATETIME_FORMAT)
+    public String deletedTime;
+
+    @Required(target = "confirmfromupdate,update,delete")
+    @IntegerType
+    public String versionNo;
+
     public void initialize() {
-        super.initialize();
+        id = null;
+        name = null;
+        value = null;
+        webCrawlingConfigId = null;
+        createdBy = null;
+        createdTime = null;
+        updatedBy = null;
+        updatedTime = null;
+        deletedBy = null;
+        deletedTime = null;
+        versionNo = null;
         createdBy = "system";
         final SimpleDateFormat sdf = new SimpleDateFormat(Constants.DEFAULT_DATETIME_FORMAT);
         createdTime = sdf.format(new Date());
