be336104652e48bb1a8d460972b43d18355305ec
Shunji Makino <makino@n2sm.net>
2015-04-17 18:53:49 +0900

    FileAuthenticationForm.java

--- src/main/java/org/codelibs/fess/web/admin/FileAuthenticationForm.java
+++ src/main/java/org/codelibs/fess/web/admin/FileAuthenticationForm.java
@@ -19,19 +19,104 @@ package org.codelibs.fess.web.admin;
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.codelibs.fess.Constants;
-import org.codelibs.fess.crud.form.admin.BsFileAuthenticationForm;
+import org.seasar.struts.annotation.DateType;
+import org.seasar.struts.annotation.IntRange;
+import org.seasar.struts.annotation.IntegerType;
+import org.seasar.struts.annotation.LongType;
+import org.seasar.struts.annotation.Maxbytelength;
+import org.seasar.struts.annotation.Required;
 
-public class FileAuthenticationForm extends BsFileAuthenticationForm implements Serializable {
+public class FileAuthenticationForm implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @Override
+    @IntegerType
+    public String pageNumber;
+
+    public Map<String, String> searchParams = new HashMap<String, String>();
+
+    @IntegerType
+    public int crudMode;
+
+    public String getCurrentPageNumber() {
+        return pageNumber;
+    }
+
+    @Required(target = "confirmfromupdate,update,delete")
+    @LongType
+    public String id;
+
+    @Maxbytelength(maxbytelength = 100)
+    public String hostname;
+
+    @IntRange(min = -1, max = 2147483647)
+    public String port;
+
+    @Maxbytelength(maxbytelength = 10)
+    public String protocolScheme;
+
+    @Required(target = "confirmfromcreate,create,confirmfromupdate,update,delete")
+    @Maxbytelength(maxbytelength = 100)
+    public String username;
+
+    @Maxbytelength(maxbytelength = 100)
+    public String password;
+
+    @Maxbytelength(maxbytelength = 1000)
+    public String parameters;
+
+    @Required(target = "confirmfromcreate,create,confirmfromupdate,update,delete")
+    @LongType
+    public String fileCrawlingConfigId;
+
+    @Required(target = "confirmfromupdate,update,delete")
+    @Maxbytelength(maxbytelength = 255)
+    public String createdBy;
+
+    @Required(target = "confirmfromupdate,update,delete")
+    @DateType(datePattern = Constants.DEFAULT_DATETIME_FORMAT)
+    public String createdTime;
+
+    @Maxbytelength(maxbytelength = 255)
+    public String updatedBy;
+
+    @DateType(datePattern = Constants.DEFAULT_DATETIME_FORMAT)
+    public String updatedTime;
+
+    @Maxbytelength(maxbytelength = 255)
+    public String deletedBy;
+
+    @DateType(datePattern = Constants.DEFAULT_DATETIME_FORMAT)
+    public String deletedTime;
+
+    @Required(target = "confirmfromupdate,update,delete")
+    @IntegerType
+    public String versionNo;
+
     public void initialize() {
-        super.initialize();
+        id = null;
+        hostname = null;
+        port = null;
+        protocolScheme = null;
+        username = null;
+        password = null;
+        parameters = null;
+        fileCrawlingConfigId = null;
+        createdBy = null;
+        createdTime = null;
+        updatedBy = null;
+        updatedTime = null;
+        deletedBy = null;
+        deletedTime = null;
+        versionNo = null;
+
         createdBy = "system";
         final SimpleDateFormat sdf = new SimpleDateFormat(Constants.DEFAULT_DATETIME_FORMAT);
         createdTime = sdf.format(new Date());
     }
+
 }
