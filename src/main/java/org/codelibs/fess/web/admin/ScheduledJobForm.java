4025ac319c285d322207c93c8b4f5c4f328d7f86
Shunji Makino <makino@n2sm.net>
2015-04-17 19:35:19 +0900

    ScheduledJobForm.java

--- src/main/java/org/codelibs/fess/web/admin/ScheduledJobForm.java
+++ src/main/java/org/codelibs/fess/web/admin/ScheduledJobForm.java
@@ -19,18 +19,111 @@ package org.codelibs.fess.web.admin;
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.codelibs.fess.Constants;
-import org.codelibs.fess.crud.form.admin.BsScheduledJobForm;
+import org.codelibs.fess.annotation.CronExpression;
+import org.seasar.struts.annotation.DateType;
+import org.seasar.struts.annotation.IntRange;
+import org.seasar.struts.annotation.IntegerType;
+import org.seasar.struts.annotation.LongType;
+import org.seasar.struts.annotation.Maxbytelength;
+import org.seasar.struts.annotation.Required;
 
-public class ScheduledJobForm extends BsScheduledJobForm implements Serializable {
+public class ScheduledJobForm implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @Override
+    @IntegerType
+    public String pageNumber;
+
+    public Map<String, String> searchParams = new HashMap<String, String>();
+
+    @IntegerType
+    public int crudMode;
+
+    public String getCurrentPageNumber() {
+        return pageNumber;
+    }
+
+    @Required(target = "confirmfromupdate,update,delete")
+    @LongType
+    public String id;
+
+    @Required(target = "confirmfromcreate,create,confirmfromupdate,update,delete")
+    @Maxbytelength(maxbytelength = 100)
+    public String name;
+
+    @Required(target = "confirmfromcreate,create,confirmfromupdate,update,delete")
+    @Maxbytelength(maxbytelength = 100)
+    public String target;
+
+    @Required(target = "confirmfromcreate,create,confirmfromupdate,update,delete")
+    @Maxbytelength(maxbytelength = 100)
+    @CronExpression
+    public String cronExpression;
+
+    @Required(target = "confirmfromcreate,create,confirmfromupdate,update,delete")
+    @Maxbytelength(maxbytelength = 100)
+    public String scriptType;
+
+    @Maxbytelength(maxbytelength = 4000)
+    public String scriptData;
+
+    // ignore
+    public String crawler;
+
+    // ignore
+    public String jobLogging;
+
+    // ignore
+    public String available;
+
+    @Required(target = "confirmfromcreate,create,confirmfromupdate,update,delete")
+    @IntegerType
+    @IntRange(min = 0, max = 2147483647)
+    public String sortOrder;
+
+    @Required(target = "confirmfromupdate,update,delete")
+    public String createdBy;
+
+    @Required(target = "confirmfromupdate,update,delete")
+    @DateType(datePattern = Constants.DEFAULT_DATETIME_FORMAT)
+    public String createdTime;
+
+    public String updatedBy;
+
+    @DateType(datePattern = Constants.DEFAULT_DATETIME_FORMAT)
+    public String updatedTime;
+
+    public String deletedBy;
+
+    @DateType(datePattern = Constants.DEFAULT_DATETIME_FORMAT)
+    public String deletedTime;
+
+    @Required(target = "confirmfromupdate,update,delete")
+    @IntegerType
+    public String versionNo;
+
     public void initialize() {
-        super.initialize();
-        super.initialize();
+        id = null;
+        name = null;
+        target = null;
+        cronExpression = null;
+        scriptType = null;
+        scriptData = null;
+        crawler = null;
+        jobLogging = null;
+        available = null;
+        sortOrder = null;
+        createdBy = null;
+        createdTime = null;
+        updatedBy = null;
+        updatedTime = null;
+        deletedBy = null;
+        deletedTime = null;
+        versionNo = null;
         target = Constants.DEFAULT_JOB_TARGET;
         cronExpression = Constants.DEFAULT_CRON_EXPRESSION;
         scriptType = Constants.DEFAULT_JOB_SCRIPT_TYPE;
