589c6fbfd53d72f2276e5dfe313c07d3b1f0ba03
Shunji Makino <makino@n2sm.net>
2015-04-22 15:38:43 +0900

    KeyMatchPager.java

--- src/main/java/org/codelibs/fess/pager/KeyMatchPager.java
+++ src/main/java/org/codelibs/fess/pager/KeyMatchPager.java
@@ -16,23 +16,131 @@
 
 package org.codelibs.fess.pager;
 
+import java.util.List;
+
 import org.codelibs.fess.Constants;
-import org.codelibs.fess.crud.pager.BsKeyMatchPager;
 
-public class KeyMatchPager extends BsKeyMatchPager {
-
-    private static final long serialVersionUID = 1L;
+public class KeyMatchPager {
 
     public KeyMatchPager() {
         super();
     }
 
-    @Override
+    public static final int DEFAULT_PAGE_SIZE = 20;
+
+    public static final int DEFAULT_CURRENT_PAGE_NUMBER = 1;
+
+    private int allRecordCount;
+
+    private int allPageCount;
+
+    private boolean existPrePage;
+
+    private boolean existNextPage;
+
+    private List<Integer> pageNumberList;
+
+    private int pageSize;
+
+    private int currentPageNumber;
+
+    public String id;
+
+    public String term;
+
+    public String query;
+
+    public String maxSize;
+
+    public String boost;
+
+    public String createdBy;
+
+    public String createdTime;
+
+    public String versionNo;
+
     public void clear() {
-        super.clear();
+        pageSize = getDefaultPageSize();
+        currentPageNumber = getDefaultCurrentPageNumber();
+
+        id = null;
+        term = null;
+        query = null;
+        maxSize = null;
+        boost = null;
+        createdBy = null;
+        createdTime = null;
+        versionNo = null;
+
     }
 
-    @Override
+    protected int getDefaultCurrentPageNumber() {
+        return DEFAULT_CURRENT_PAGE_NUMBER;
+    }
+
+    public int getAllRecordCount() {
+        return allRecordCount;
+    }
+
+    public void setAllRecordCount(final int allRecordCount) {
+        this.allRecordCount = allRecordCount;
+    }
+
+    public int getAllPageCount() {
+        return allPageCount;
+    }
+
+    public void setAllPageCount(final int allPageCount) {
+        this.allPageCount = allPageCount;
+    }
+
+    public boolean isExistPrePage() {
+        return existPrePage;
+    }
+
+    public void setExistPrePage(final boolean existPrePage) {
+        this.existPrePage = existPrePage;
+    }
+
+    public boolean isExistNextPage() {
+        return existNextPage;
+    }
+
+    public void setExistNextPage(final boolean existNextPage) {
+        this.existNextPage = existNextPage;
+    }
+
+    public int getPageSize() {
+        if (pageSize <= 0) {
+            pageSize = getDefaultPageSize();
+        }
+        return pageSize;
+    }
+
+    public void setPageSize(final int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getCurrentPageNumber() {
+        if (currentPageNumber <= 0) {
+            currentPageNumber = getDefaultCurrentPageNumber();
+        }
+        return currentPageNumber;
+    }
+
+    public void setCurrentPageNumber(final int currentPageNumber) {
+        this.currentPageNumber = currentPageNumber;
+    }
+
+    public List<Integer> getPageNumberList() {
+        return pageNumberList;
+    }
+
+    public void setPageNumberList(final List<Integer> pageNumberList) {
+        this.pageNumberList = pageNumberList;
+    }
+
     protected int getDefaultPageSize() {
         return Constants.DEFAULT_ADMIN_PAGE_SIZE;
     }
