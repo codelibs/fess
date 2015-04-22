d1a2e1178540fdd9e1caa3d0d0f6c9b0e8f0636f
Shunji Makino <makino@n2sm.net>
2015-04-22 16:19:51 +0900

    WebCrawlingConfigPager.java

--- src/main/java/org/codelibs/fess/pager/WebCrawlingConfigPager.java
+++ src/main/java/org/codelibs/fess/pager/WebCrawlingConfigPager.java
@@ -16,23 +16,143 @@
 
 package org.codelibs.fess.pager;
 
+import java.util.List;
+
 import org.codelibs.fess.Constants;
-import org.codelibs.fess.crud.pager.BsWebCrawlingConfigPager;
 
-public class WebCrawlingConfigPager extends BsWebCrawlingConfigPager {
+public class WebCrawlingConfigPager {
 
-    private static final long serialVersionUID = 1L;
+    public static final int DEFAULT_PAGE_SIZE = 20;
+
+    public static final int DEFAULT_CURRENT_PAGE_NUMBER = 1;
+
+    private int allRecordCount;
+
+    private int allPageCount;
+
+    private boolean existPrePage;
+
+    private boolean existNextPage;
+
+    private List<Integer> pageNumberList;
+
+    private int pageSize;
+
+    private int currentPageNumber;
+
+    public String id;
+
+    public String name;
+
+    public String urls;
+
+    public String userAgent;
+
+    public String numOfThread;
+
+    public String intervalTime;
+
+    public String boost;
+
+    public String available;
+
+    public String sortOrder;
+
+    public String createdBy;
+
+    public String createdTime;
+
+    public String versionNo;
+
+    public void clear() {
+        pageSize = getDefaultPageSize();
+        currentPageNumber = getDefaultCurrentPageNumber();
+
+        id = null;
+        name = null;
+        urls = null;
+        userAgent = null;
+        numOfThread = null;
+        intervalTime = null;
+        boost = null;
+        available = null;
+        sortOrder = null;
+        createdBy = null;
+        createdTime = null;
+        versionNo = null;
+
+    }
+
+    protected int getDefaultCurrentPageNumber() {
+        return DEFAULT_CURRENT_PAGE_NUMBER;
+    }
+
+    public int getAllRecordCount() {
+        return allRecordCount;
+    }
+
+    public void setAllRecordCount(final int allRecordCount) {
+        this.allRecordCount = allRecordCount;
+    }
+
+    public int getAllPageCount() {
+        return allPageCount;
+    }
+
+    public void setAllPageCount(final int allPageCount) {
+        this.allPageCount = allPageCount;
+    }
+
+    public boolean isExistPrePage() {
+        return existPrePage;
+    }
+
+    public void setExistPrePage(final boolean existPrePage) {
+        this.existPrePage = existPrePage;
+    }
+
+    public boolean isExistNextPage() {
+        return existNextPage;
+    }
+
+    public void setExistNextPage(final boolean existNextPage) {
+        this.existNextPage = existNextPage;
+    }
+
+    public int getPageSize() {
+        if (pageSize <= 0) {
+            pageSize = getDefaultPageSize();
+        }
+        return pageSize;
+    }
+
+    public void setPageSize(final int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getCurrentPageNumber() {
+        if (currentPageNumber <= 0) {
+            currentPageNumber = getDefaultCurrentPageNumber();
+        }
+        return currentPageNumber;
+    }
+
+    public void setCurrentPageNumber(final int currentPageNumber) {
+        this.currentPageNumber = currentPageNumber;
+    }
+
+    public List<Integer> getPageNumberList() {
+        return pageNumberList;
+    }
+
+    public void setPageNumberList(final List<Integer> pageNumberList) {
+        this.pageNumberList = pageNumberList;
+    }
 
     public WebCrawlingConfigPager() {
         super();
     }
 
-    @Override
-    public void clear() {
-        super.clear();
-    }
-
-    @Override
     protected int getDefaultPageSize() {
         return Constants.DEFAULT_ADMIN_PAGE_SIZE;
     }
