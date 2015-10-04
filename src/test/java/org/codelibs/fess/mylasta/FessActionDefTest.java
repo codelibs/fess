ccd9a3a3714e49d4241c704b2a2cf3bb8cebe633
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2015-10-04 22:52:10 +0900

    add test cases

---
+++ src/test/java/org/codelibs/fess/mylasta/FessActionDefTest.java
@@ -0,0 +1,46 @@
+package org.codelibs.fess.mylasta;
+
+import org.codelibs.fess.unit.UnitFessTestCase;
+
+public class FessActionDefTest extends UnitFessTestCase {
+
+    // ===================================================================================
+    //                                                                           Component
+    //                                                                           =========
+    /*
+    public void test_component() throws Exception {
+        // ## Arrange ##
+        String appWebPkg = ".app.web.";
+        String actionSuffix = "Action";
+    
+        // ## Act ##
+        policeStoryOfJavaClassChase((srcFile, clazz) -> {
+            if (clazz.isInterface() || Modifier.isAbstract(clazz.getModifiers())) { // e.g. BaseAction
+                return;
+            }
+            final String className = clazz.getName();
+            if (className.contains(appWebPkg) && className.endsWith(actionSuffix)) {
+                // ## Assert ##
+                markHere("exists");
+                getComponent(clazz); // expect no exception
+            }
+        });
+        assertMarked("exists");
+    }
+    */
+
+    // ===================================================================================
+    //                                                                            Document
+    //                                                                            ========
+    /*
+    public void test_document() throws Exception {
+        DocumentGenerator documentGenerator = new DocumentGenerator();
+        documentGenerator.saveLastaDocMeta();
+    }
+    */
+
+    public void test_dummy() throws Exception {
+        // TODO remove
+        assertTrue(true);
+    }
+}
