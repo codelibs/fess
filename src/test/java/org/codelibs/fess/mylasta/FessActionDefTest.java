868aee6949351866b549383b8158b4bd03ba2878
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2015-12-28 10:46:25 +0900

    update lastaflute and utflute

---
+++ src/test/java/org/codelibs/fess/mylasta/FessActionDefTest.java
@@ -0,0 +1,26 @@
+package org.codelibs.fess.mylasta;
+
+import org.codelibs.fess.unit.UnitFessTestCase;
+
+public class FessActionDefTest extends UnitFessTestCase {
+
+    public void test_component() throws Exception {
+        //        policeStoryOfJavaClassChase(new ActionComponentPolice(tp -> getComponent(tp)));
+    }
+
+    public void test_hotDeployDestroyer() throws Exception {
+        //        policeStoryOfJavaClassChase(new HotDeployDestroyerPolice(tp -> getComponent(tp)));
+    }
+
+    public void test_nonActionExtendsAction() throws Exception {
+        //        policeStoryOfJavaClassChase(new NonActionExtendsActionPolice());
+    }
+
+    public void test_nonWebHasWebReference() throws Exception {
+        //        policeStoryOfJavaClassChase(new NonWebHasWebReferencePolice());
+    }
+
+    public void test_webPackageNinjaReferencePolice() throws Exception {
+        //        policeStoryOfJavaClassChase(new WebPackageNinjaReferencePolice());
+    }
+}
