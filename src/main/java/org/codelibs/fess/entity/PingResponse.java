05506acc7a454b3d82bc0d91e0c9f51da41cb3be
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2016-03-29 06:44:32 +0900

    fix #455 : support es 2.2

--- src/main/java/org/codelibs/fess/entity/PingResponse.java
+++ src/main/java/org/codelibs/fess/entity/PingResponse.java
@@ -16,7 +16,7 @@
 package org.codelibs.fess.entity;
 
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
-import org.elasticsearch.action.admin.cluster.health.ClusterHealthStatus;
+import org.elasticsearch.cluster.health.ClusterHealthStatus;
 
 public class PingResponse {
     private int status = 0;
