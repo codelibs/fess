59b03b931c118d70773b150f7f734688eb2858bf
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2016-05-14 16:31:06 +0900

    change to constant

--- src/main/java/org/codelibs/fess/job/CrawlJob.java
+++ src/main/java/org/codelibs/fess/job/CrawlJob.java
@@ -43,6 +43,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CrawlJob {
+    private static final String XDEBUG_XRUNJDWP_TRANSPORT_DT_SOCKET_SERVER_Y_SUSPEND_Y_ADDRESS_127_0_0_1_8000 =
+            "-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=127.0.0.1:8000";
+
     private static final Logger logger = LoggerFactory.getLogger(CrawlJob.class);
 
     protected JobExecutor jobExecutor;
@@ -137,7 +140,7 @@ public class CrawlJob {
     }
 
     public CrawlJob remoteDebug() {
-        return jvmOptions("-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=127.0.0.1:8000");
+        return jvmOptions(XDEBUG_XRUNJDWP_TRANSPORT_DT_SOCKET_SERVER_Y_SUSPEND_Y_ADDRESS_127_0_0_1_8000);
     }
 
     public CrawlJob jvmOptions(String option) {
