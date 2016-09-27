61b081110eb0b4ad247c7074d10a7b0375401c3e
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2016-09-28 06:36:40 +0900

    modify labels

--- src/main/java/org/codelibs/fess/mylasta/action/FessLabels.java
+++ src/main/java/org/codelibs/fess/mylasta/action/FessLabels.java
@@ -730,15 +730,15 @@ public class FessLabels extends UserMessages {
 
     /** The key of the message: iBooks */
     public static final String LABELS_facet_filetype_ibooks = "{labels.facet_filetype_ibooks}";
-	
-	/** The key of the message: Plain text */
-	public static final String LABELS_facet_filetype_txt = "{labels.facet_filetype_txt}";
 
-	/** The key of the message: Rich Text Format */
-	public static final String LABELS_facet_filetype_rtf = "{labels.facet_filetype_rtf}";
-  	
-	/** The key of the message: Help Format */
-	public static final String LABELS_facet_filetype_chm = "{labels.facet_filetype_chm}";
+    /** The key of the message: Plain Text */
+    public static final String LABELS_facet_filetype_txt = "{labels.facet_filetype_txt}";
+
+    /** The key of the message: Rich Text Format */
+    public static final String LABELS_facet_filetype_rtf = "{labels.facet_filetype_rtf}";
+
+    /** The key of the message: Compiled HTML Help */
+    public static final String LABELS_facet_filetype_chm = "{labels.facet_filetype_chm}";
 
     /** The key of the message: Others */
     public static final String LABELS_facet_filetype_others = "{labels.facet_filetype_others}";
