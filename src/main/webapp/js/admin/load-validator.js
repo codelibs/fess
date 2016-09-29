174b16ec880b307057b49aecae8ce91d7216b74b
matsutani <matsutani@n2sm.net>
2016-09-29 10:14:26 +0900

    #636 [WIP] admin validate

--- src/main/webapp/js/admin/load-validator.js
+++ src/main/webapp/js/admin/load-validator.js
@@ -1,19 +1,19 @@
 $(function() {
-	var SUBSTR_START = 0;
-	var SUBSTR_END = 2;
 	var SEP_CHAR = '-'
-	var sepLangCode = 'en';
+	var langCode;
 	var browserLang = (window.navigator.languages && window.navigator.languages[0])
 			|| window.navigator.language
 			|| window.navigator.userLanguage
 			|| window.navigator.browserLanguage;
 
 	if (browserLang) {
-		sepLangCode = browserLang.split(SEP_CHAR);
-	};
+		langCode = browserLang.split(SEP_CHAR)[0];
+	} else {
+		langCode = 'en';
+	}
 
 	$.validate({
 		modules : 'html5',
-		lang : sepLangCode,
+		lang : langCode,
 	});
 });
