1dc6ab01a1dad1ffeb16632069d54db5eb8240a2
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2017-02-02 22:38:02 +0900

    fix #867 update copyright

--- src/main/java/org/codelibs/fess/mylasta/action/FessLabels.java
+++ src/main/java/org/codelibs/fess/mylasta/action/FessLabels.java
@@ -584,7 +584,7 @@ public class FessLabels extends UserMessages {
     /** The key of the message: MENU */
     public static final String LABELS_SIDEBAR_MENU = "{labels.sidebar.menu}";
 
-    /** The key of the message: Copyright(C) 2009-2016 &lt;a href="https://github.com/codelibs"&gt;CodeLibs Project&lt;/a&gt;. &lt;span class="br-phone"&gt;&lt;/span&gt;All Rights Reserved. */
+    /** The key of the message: Copyright(C) 2009-2017 &lt;a href="https://github.com/codelibs"&gt;CodeLibs Project&lt;/a&gt;. &lt;span class="br-phone"&gt;&lt;/span&gt;All Rights Reserved. */
     public static final String LABELS_FOOTER_COPYRIGHT = "{labels.footer.copyright}";
 
     /** The key of the message: Search */
