24db739fdbb9a10646d929a13f01858231802dda
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2017-04-07 23:37:24 +0900

    modify imports

--- src/main/java/org/codelibs/fess/helper/DocumentHelper.java
+++ src/main/java/org/codelibs/fess/helper/DocumentHelper.java
@@ -22,7 +22,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StringReader;
-import java.io.UnsupportedEncodingException;
 import java.util.Base64;
 import java.util.HashSet;
 import java.util.Map;
