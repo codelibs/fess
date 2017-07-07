497c3a6d2998e86ba604beb8a51310436ef7025b
Shinsuke Sugaya <shinsuke@apache.org>
2017-07-07 22:53:18 +0900

    add asEsIndex method

--- src/main/java/org/codelibs/fess/es/config/exbhv/RelatedQueryBhv.java
+++ src/main/java/org/codelibs/fess/es/config/exbhv/RelatedQueryBhv.java
@@ -16,10 +16,14 @@
 package org.codelibs.fess.es.config.exbhv;
 
 import org.codelibs.fess.es.config.bsbhv.BsRelatedQueryBhv;
+import org.codelibs.fess.util.ComponentUtil;
 
 /**
  * @author FreeGen
  */
 public class RelatedQueryBhv extends BsRelatedQueryBhv {
-
+    @Override
+    protected String asEsIndex() {
+        return ComponentUtil.getFessConfig().getIndexConfigIndex();
+    }
 }
