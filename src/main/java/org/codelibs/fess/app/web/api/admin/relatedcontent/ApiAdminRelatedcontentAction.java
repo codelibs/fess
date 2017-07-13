c2dd9f44f2d62a6a8a5f47bd8491272711627dc1
nullpos <greenstar77@gmail.com>
2017-07-13 18:28:01 +0900

    fix #1148

---
+++ src/main/java/org/codelibs/fess/app/web/api/admin/relatedcontent/ApiAdminRelatedcontentAction.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2012-2017 CodeLibs Project and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.codelibs.fess.app.web.api.admin.relatedcontent;
+
+import static org.codelibs.fess.app.web.admin.relatedcontent.AdminRelatedcontentAction.getRelatedContent;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.annotation.Resource;
+
+import org.codelibs.fess.app.pager.RelatedContentPager;
+import org.codelibs.fess.app.service.RelatedContentService;
+import org.codelibs.fess.app.web.CrudMode;
+import org.codelibs.fess.app.web.api.ApiResult;
+import org.codelibs.fess.app.web.api.ApiResult.ApiConfigResponse;
+import org.codelibs.fess.app.web.api.ApiResult.ApiResponse;
+import org.codelibs.fess.app.web.api.ApiResult.ApiUpdateResponse;
+import org.codelibs.fess.app.web.api.ApiResult.Status;
+import org.codelibs.fess.app.web.api.admin.FessApiAdminAction;
+import org.codelibs.fess.es.config.exentity.RelatedContent;
+import org.lastaflute.web.Execute;
+import org.lastaflute.web.response.JsonResponse;
+
+public class ApiAdminRelatedcontentAction extends FessApiAdminAction {
+
+    // ===================================================================================
+    //                                                                           Attribute
+    //                                                                           =========
+    @Resource
+    private RelatedContentService relatedContentService;
+
+    // ===================================================================================
+    //                                                                      Search Execute
+    //                                                                      ==============
+
+    // GET /api/admin/relatedcontent/settings
+    // POST /api/admin/relatedcontent/settings
+    @Execute
+    public JsonResponse<ApiResult> settings(final SearchBody body) {
+        validateApi(body, messages -> {});
+        final RelatedContentPager pager = copyBeanToNewBean(body, RelatedContentPager.class);
+        final List<RelatedContent> list = relatedContentService.getRelatedContentList(pager);
+        return asJson(new ApiResult.ApiConfigsResponse<EditBody>()
+                .settings(list.stream().map(entity -> createEditBody(entity)).collect(Collectors.toList()))
+                .total(pager.getAllRecordCount()).status(ApiResult.Status.OK).result());
+    }
+
+    // GET /api/admin/relatedcontent/setting/{id}
+    @Execute
+    public JsonResponse<ApiResult> get$setting(final String id) {
+        return asJson(new ApiConfigResponse()
+                .setting(relatedContentService.getRelatedContent(id).map(entity -> createEditBody(entity)).orElseGet(() -> {
+                    throwValidationErrorApi(messages -> messages.addErrorsCrudCouldNotFindCrudTable(GLOBAL, id));
+                    return null;
+                })).status(Status.OK).result());
+    }
+
+    // PUT /api/admin/relatedcontent/setting
+    @Execute
+    public JsonResponse<ApiResult> put$setting(final CreateBody body) {
+        validateApi(body, messages -> {});
+        body.crudMode = CrudMode.CREATE;
+        final RelatedContent relatedContent = getRelatedContent(body).map(entity -> {
+            try {
+                relatedContentService.store(entity);
+            } catch (final Exception e) {
+                throwValidationErrorApi(messages -> messages.addErrorsCrudFailedToCreateCrudTable(GLOBAL, buildThrowableMessage(e)));
+            }
+            return entity;
+        }).orElseGet(() -> {
+            throwValidationErrorApi(messages -> messages.addErrorsCrudFailedToCreateInstance(GLOBAL));
+            return null;
+        });
+
+        return asJson(new ApiUpdateResponse().id(relatedContent.getId()).created(true).status(Status.OK).result());
+    }
+
+    // POST /api/admin/relatedcontent/setting
+    @Execute
+    public JsonResponse<ApiResult> post$setting(final EditBody body) {
+        validateApi(body, messages -> {});
+        body.crudMode = CrudMode.EDIT;
+        final RelatedContent relatedContent = getRelatedContent(body).map(entity -> {
+            try {
+                relatedContentService.store(entity);
+            } catch (final Exception e) {
+                throwValidationErrorApi(messages -> messages.addErrorsCrudFailedToUpdateCrudTable(GLOBAL, buildThrowableMessage(e)));
+            }
+            return entity;
+        }).orElseGet(() -> {
+            throwValidationErrorApi(messages -> messages.addErrorsCrudCouldNotFindCrudTable(GLOBAL, body.id));
+            return null;
+        });
+        return asJson(new ApiUpdateResponse().id(relatedContent.getId()).created(false).status(Status.OK).result());
+    }
+
+    // DELETE /api/admin/relatedcontent/setting/{id}
+    @Execute
+    public JsonResponse<ApiResult> delete$setting(final String id) {
+        relatedContentService.getRelatedContent(id).ifPresent(entity -> {
+            try {
+                relatedContentService.delete(entity);
+                saveInfo(messages -> messages.addSuccessCrudDeleteCrudTable(GLOBAL));
+            } catch (final Exception e) {
+                throwValidationErrorApi(messages -> messages.addErrorsCrudFailedToDeleteCrudTable(GLOBAL, buildThrowableMessage(e)));
+            }
+        }).orElse(() -> {
+            throwValidationErrorApi(messages -> messages.addErrorsCrudCouldNotFindCrudTable(GLOBAL, id));
+        });
+        return asJson(new ApiResponse().status(Status.OK).result());
+    }
+
+    protected EditBody createEditBody(final RelatedContent entity) {
+        final EditBody body = new EditBody();
+        copyBeanToBean(entity, body, copyOp -> {
+            copyOp.excludeNull();
+        });
+        return body;
+    }
+}
