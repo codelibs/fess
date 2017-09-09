c45bfa9c41a0f729344fd8acff144ba537bc4266
Shinsuke Sugaya <shinsuke@apache.org>
2017-09-09 09:21:13 +0900

    update travis testing

---
+++ .travis/before_script.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+TMP_FILE=/tmp/fess-build.$$
+unzip target/releases/fess-*.zip 2>&1 > $TMP_FILE
+tail $TMP_FILE
+
+./fess-*/bin/fess 2>&1 > $TMP_FILE &
+sleep 3
+tail $TMP_FILE
+tail ./fess-*/logs/*.log &
+
+counter=0
+ret=1
+while [ $ret != 0 -a $counter != 60 ] ; do
+  echo "Ping Fess... $counter"
+  curl -v 127.0.0.1:8080/json/?type=ping
+  #ret=$?
+  ret=1
+  sleep 5
+  counter=`expr $counter + 1`
+done
+
