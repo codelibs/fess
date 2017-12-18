aacf6e2da268cb8dddf0e06793f47e2eb3a8d8a6
Shinsuke Sugaya <shinsuke@apache.org>
2017-12-18 23:16:20 +0900

    update dependencies

--- src/main/java/org/codelibs/fess/app/web/base/FessSearchAction.java
+++ src/main/java/org/codelibs/fess/app/web/base/FessSearchAction.java
@@ -218,7 +218,7 @@ public abstract class FessSearchAction extends FessBaseAction {
 
     protected HtmlResponse redirectToRoot() {
         final String contextPath = request.getServletContext().getContextPath();
-        return newHtmlResponseAsRediect(StringUtil.isBlank(contextPath) ? "/" : contextPath);
+        return newHtmlResponseAsRedirect(StringUtil.isBlank(contextPath) ? "/" : contextPath);
     }
 
     protected HtmlNext virtualHost(final HtmlNext path) {
