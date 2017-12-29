85d265184bdaaaf795ff5bc74daf1e1a9bb3ee7e
Shinsuke Sugaya <shinsuke@apache.org>
2017-12-29 21:09:54 +0900

    fix #1405 wrap redirect responses

--- src/main/java/org/codelibs/fess/app/web/base/FessSearchAction.java
+++ src/main/java/org/codelibs/fess/app/web/base/FessSearchAction.java
@@ -213,12 +213,13 @@ public abstract class FessSearchAction extends FessBaseAction {
     }
 
     protected HtmlResponse redirectToLogin() {
-        return redirect(SsoAction.class);
+        return systemHelper.getRedirectResponseToLogin(request, redirect(SsoAction.class));
     }
 
     protected HtmlResponse redirectToRoot() {
         final String contextPath = request.getServletContext().getContextPath();
-        return newHtmlResponseAsRediect(StringUtil.isBlank(contextPath) ? "/" : contextPath);
+        return systemHelper.getRedirectResponseToRoot(request,
+                newHtmlResponseAsRediect(StringUtil.isBlank(contextPath) ? "/" : contextPath));
     }
 
     protected HtmlNext virtualHost(final HtmlNext path) {
