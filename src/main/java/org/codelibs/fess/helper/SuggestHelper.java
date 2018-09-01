f8a21de6c2aed4999b222e0369f741c30e31961b
Shinsuke Sugaya <shinsuke@apache.org>
2018-09-01 10:29:58 +0900

    code clean up

--- src/main/java/org/codelibs/fess/helper/SuggestHelper.java
+++ src/main/java/org/codelibs/fess/helper/SuggestHelper.java
@@ -23,11 +23,11 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.Map;
-import java.util.HashMap;
+import java.util.Set;
 import java.util.function.Consumer;
 
 import javax.annotation.PostConstruct;
