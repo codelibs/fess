5da01723e13a38b988347c6ff5cf62bd0b523bef
Shinsuke Sugaya <shinsuke@apache.org>
2019-02-02 18:02:46 +0900

    fix #2003 add date type

--- src/main/java/org/codelibs/fess/Constants.java
+++ src/main/java/org/codelibs/fess/Constants.java
@@ -398,6 +398,8 @@ public class Constants extends CoreLibConstants {
 
     public static final String MAPPING_TYPE_DOUBLE = "double";
 
+    public static final String MAPPING_TYPE_DATE = "date";
+
     public static final String PAGING_QUERY_LIST = "pagingQueryList";
 
     public static final String REQUEST_LANGUAGES = "requestLanguages";
