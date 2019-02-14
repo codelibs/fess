0d774a81607a4f9ae40e296b4bf615419e5eca12
Shinsuke Sugaya <shinsuke@apache.org>
2019-02-14 19:06:44 +0900

    #2019 update settings

--- src/main/java/org/codelibs/fess/mylasta/direction/FessConfig.java
+++ src/main/java/org/codelibs/fess/mylasta/direction/FessConfig.java
@@ -288,8 +288,8 @@ public interface FessConfig extends FessEnv, org.codelibs.fess.mylasta.direction
 
     /** The key of the configuration. e.g. title=title:string
     Title=title:string
-    last_modified=Last-Save-Date:date
-    last_modified=Last-Modified:date
+    Last-Save-Date=last_modified:date
+    Last-Modified=last_modified:date
     */
     String CRAWLER_METADATA_NAME_MAPPING = "crawler.metadata.name.mapping";
 
@@ -2210,8 +2210,8 @@ public interface FessConfig extends FessEnv, org.codelibs.fess.mylasta.direction
      * Get the value for the key 'crawler.metadata.name.mapping'. <br>
      * The value is, e.g. title=title:string
     Title=title:string
-    last_modified=Last-Save-Date:date
-    last_modified=Last-Modified:date
+    Last-Save-Date=last_modified:date
+    Last-Modified=last_modified:date
     <br>
      * @return The value of found property. (NotNull: if not found, exception but basically no way)
      */
@@ -8620,7 +8620,7 @@ public interface FessConfig extends FessEnv, org.codelibs.fess.mylasta.direction
             defaultMap.put(FessConfig.CRAWLER_SYSTEM_MONITOR_INTERVAL, "60");
             defaultMap.put(FessConfig.CRAWLER_METADATA_CONTENT_EXCLUDES, "resourceName,X-Parsed-By,Content-Encoding.*,Content-Type.*");
             defaultMap.put(FessConfig.CRAWLER_METADATA_NAME_MAPPING,
-                    "title=title:string\nTitle=title:string\nlast_modified=Last-Save-Date:date\nlast_modified=Last-Modified:date\n");
+                    "title=title:string\nTitle=title:string\nLast-Save-Date=last_modified:date\nLast-Modified=last_modified:date\n");
             defaultMap.put(FessConfig.CRAWLER_DOCUMENT_HTML_CONTENT_XPATH, "//BODY");
             defaultMap.put(FessConfig.CRAWLER_DOCUMENT_HTML_LANG_XPATH, "//HTML/@lang");
             defaultMap.put(FessConfig.CRAWLER_DOCUMENT_HTML_DIGEST_XPATH, "//META[@name='description']/@content");
