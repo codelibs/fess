f690f47ce993a7bd08f41d121183f6f352ecd446
Shinsuke Sugaya <shinsuke@apache.org>
2019-04-02 06:37:49 +0900

    #2075 fix typo

--- src/main/java/org/codelibs/fess/expression/engine/SaiExpressionEngine.java
+++ src/main/java/org/codelibs/fess/expression/engine/SaiExpressionEngine.java
@@ -13,7 +13,7 @@
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-package org.codelibs.fessexpression.engine;
+package org.codelibs.fess.expression.engine;
 
 import java.util.Map;
 
