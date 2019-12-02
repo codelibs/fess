5cf6593ddc65185ed5cbdf366259f2988df557e7
Shinsuke Sugaya <shinsuke@apache.org>
2019-12-03 05:49:00 +0900

    fix test cases

--- src/test/java/org/codelibs/fess/it/admin/FileAuthTests.java
+++ src/test/java/org/codelibs/fess/it/admin/FileAuthTests.java
@@ -106,7 +106,7 @@ public class FileAuthTests extends CrudTestBase {
     @Override
     protected Map<String, Object> getUpdateMap() {
         final Map<String, Object> updateMap = new HashMap<>();
-        updateMap.put("parameters", "new_parameters");
+        updateMap.put("parameters", "new_parameters=test");
         return updateMap;
     }
 
