c3b1035eaf5231766df4697ada02c4eb04564b0d
Shinsuke Sugaya <shinsuke@apache.org>
2019-12-28 19:12:25 +0900

    fix #2350 add fe:maskEmail

--- src/main/java/org/codelibs/fess/taglib/FessFunctions.java
+++ src/main/java/org/codelibs/fess/taglib/FessFunctions.java
@@ -74,6 +74,9 @@ public class FessFunctions {
 
     private static final String PDF_DATE = "pdf_date";
 
+    private static final Pattern EMAIL_ADDRESS_PATTERN = Pattern.compile("[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}",
+            Pattern.CASE_INSENSITIVE);
+
     private static LoadingCache<String, Long> resourceHashCache = CacheBuilder.newBuilder().maximumSize(1000)
             .expireAfterWrite(10, TimeUnit.MINUTES).build(new CacheLoader<String, Long>() {
                 @Override
@@ -418,5 +421,12 @@ public class FessFunctions {
         return loginAssist.getSavedUserBean()
                 .map(user -> user.hasRoles(roles) || user.hasRoles(ComponentUtil.getFessConfig().getAuthenticationAdminRolesAsArray()))
                 .orElse(false);
+    }
+
+    public static String maskEmail(final String value) {
+        if (value == null) {
+            return StringUtil.EMPTY;
+        }
+        return EMAIL_ADDRESS_PATTERN.matcher(value).replaceAll("******@****.***");
     }
 }
