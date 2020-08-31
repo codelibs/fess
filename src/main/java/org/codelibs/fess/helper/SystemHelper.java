5b568e4cb92961694fe716ccd8502b3a838d8e93
Shinsuke Sugaya <shinsuke@apache.org>
2020-09-01 06:45:54 +0900

    fix #2477 update eol date

--- src/main/java/org/codelibs/fess/helper/SystemHelper.java
+++ src/main/java/org/codelibs/fess/helper/SystemHelper.java
@@ -124,7 +124,7 @@ public class SystemHelper {
             logger.debug("Initialize {}", this.getClass().getSimpleName());
         }
         final Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-        cal.set(2021, 12 - 1, 18); // EOL Date
+        cal.set(2022, 2 - 1, 18); // EOL Date
         eolTime = cal.getTimeInMillis();
         if (isEoled()) {
             logger.error("Your system is out of support. See https://fess.codelibs.org/eol.html");
