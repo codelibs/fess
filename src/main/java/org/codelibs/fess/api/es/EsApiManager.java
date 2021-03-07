9b6ec3a8f3180b37208f4b1a96646eb451a0ba16
Shinsuke Sugaya <shinsuke@apache.org>
2021-03-07 17:54:55 +0900

    fix #2540 check a size of request body

--- src/main/java/org/codelibs/fess/api/es/EsApiManager.java
+++ src/main/java/org/codelibs/fess/api/es/EsApiManager.java
@@ -141,7 +141,7 @@ public class EsApiManager extends BaseApiManager {
         });
         try (final CurlResponse curlResponse = curlRequest.onConnect((req, con) -> {
             con.setDoOutput(true);
-            if (httpMethod != Method.GET) {
+            if (httpMethod != Method.GET && request.getContentLength() > 2) {
                 try (ServletInputStream in = request.getInputStream(); OutputStream out = con.getOutputStream()) {
                     CopyUtil.copy(in, out);
                 } catch (final IOException e) {
