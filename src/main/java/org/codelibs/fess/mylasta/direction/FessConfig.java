37edba8802496e467f97b07f6e36ecee2542e824
Shinsuke Sugaya <shinsuke@apache.org>
2022-12-15 10:11:49 +0900

    fix #2699 add dc:title

--- src/main/java/org/codelibs/fess/mylasta/direction/FessConfig.java
+++ src/main/java/org/codelibs/fess/mylasta/direction/FessConfig.java
@@ -390,6 +390,7 @@ public interface FessConfig extends FessEnv, org.codelibs.fess.mylasta.direction
 
     /** The key of the configuration. e.g. title=title:string<br>
      * Title=title:string<br>
+     * dc:title=title:string<br>
      *  */
     String CRAWLER_METADATA_NAME_MAPPING = "crawler.metadata.name.mapping";
 
@@ -2857,6 +2858,7 @@ public interface FessConfig extends FessEnv, org.codelibs.fess.mylasta.direction
      * Get the value for the key 'crawler.metadata.name.mapping'. <br>
      * The value is, e.g. title=title:string<br>
      * Title=title:string<br>
+     * dc:title=title:string<br>
      *  <br>
      * @return The value of found property. (NotNull: if not found, exception but basically no way)
      */
@@ -10538,7 +10540,7 @@ public interface FessConfig extends FessEnv, org.codelibs.fess.mylasta.direction
             defaultMap.put(FessConfig.CRAWLER_HOTTHREAD_TYPE, "cpu");
             defaultMap.put(FessConfig.CRAWLER_METADATA_CONTENT_EXCLUDES,
                     "resourceName,X-Parsed-By,Content-Encoding.*,Content-Type.*,X-TIKA.*");
-            defaultMap.put(FessConfig.CRAWLER_METADATA_NAME_MAPPING, "title=title:string\nTitle=title:string\n");
+            defaultMap.put(FessConfig.CRAWLER_METADATA_NAME_MAPPING, "title=title:string\nTitle=title:string\ndc:title=title:string\n");
             defaultMap.put(FessConfig.CRAWLER_DOCUMENT_HTML_CONTENT_XPATH, "//BODY");
             defaultMap.put(FessConfig.CRAWLER_DOCUMENT_HTML_LANG_XPATH, "//HTML/@lang");
             defaultMap.put(FessConfig.CRAWLER_DOCUMENT_HTML_DIGEST_XPATH, "//META[@name='description']/@content");
