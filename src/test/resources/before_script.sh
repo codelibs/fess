d15697990417819487024d8a902b16d821fd3736
Shinsuke Sugaya <shinsuke@apache.org>
2024-02-09 21:38:41 +0900

    update script

--- src/test/resources/before_script.sh
+++ src/test/resources/before_script.sh
@@ -6,32 +6,34 @@ unzip target/releases/fess-*.zip > ${temp_file} 2>&1
 tail ${temp_file}
 
 ./fess-*/bin/fess > ${temp_file} 2>&1 &
-fess_pid=$!
+
+counter=0
+while [[ ! -f ./fess-*/logs/fess.log  && $counter -lt 30 ]]; do
+  sleep 1
+  ((counter++))
+done
 
 pushd /tmp >/dev/null
 git clone https://github.com/codelibs/fess-testdata.git
 popd >/dev/null
 
-tail ${temp_file}
 touch $(ls -d ./fess-*/logs)/fess-crawler.log
-tail -f ./fess-*/logs/*.log &
+tail -f ${temp_file} ./fess-*/logs/*.log &
 
 response_file=/tmp/response.$$
+touch ${response_file}
+
 counter=0
 ret=1
 while [[ $ret != 0 && $counter -lt 180 ]]; do
   echo "Ping Fess... $counter"
-  if ! curl -o ${response_file} -v "localhost:8080/api/v1/health"; then
-    ret=$?
-  else
+  curl -o ${response_file} -v "localhost:8080/api/v1/health"
+  cat ${response_file}
+  if grep green ${response_file} >/dev/null; then
     ret=0
   fi
   sleep 1
   ((counter++))
 done
 
-cat ${response_file}
-if ! grep green ${response_file} >/dev/null; then
-  exit $?
-fi
 
