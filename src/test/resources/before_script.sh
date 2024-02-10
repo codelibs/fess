0494e440b1269cd6c4babf7740c05c606654183a
Shinsuke Sugaya <shinsuke@apache.org>
2024-02-10 12:19:37 +0900

    Refactor GitHub Actions for Improved Efficiency and Maintenance (#2805)

--- src/test/resources/before_script.sh
+++ src/test/resources/before_script.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
+set -xuo pipefail
 
-TMP_FILE=/tmp/fess-build.$$
-unzip target/releases/fess-*.zip 2>&1 > $TMP_FILE
-tail $TMP_FILE
+temp_log_file=/tmp/fess-build.$$
+unzip target/releases/fess-*.zip > ${temp_log_file} 2>&1
+tail ${temp_log_file}
 
-./fess-*/bin/fess 2>&1 > $TMP_FILE &
+./fess-*/bin/fess > ${temp_log_file} 2>&1 &
 
-pushd /tmp
-git clone https://github.com/codelibs/fess-testdata.git
-popd
-
-tail $TMP_FILE
-touch `ls -d ./fess-*/logs`/fess-crawler.log
-tail -f ./fess-*/logs/*.log &
-
-counter=0
-ret=1
-while [ $ret != 0 -a $counter != 180 ] ; do
-  echo "Ping Fess... $counter"
-  curl -v "localhost:8080/json/?type=ping"
-  ret=$?
+temp_json_file=/tmp/fess-log.$$
+touch ${temp_json_file}
+error_count=0
+while true ; do
+  status=$(curl -w '%{http_code}\n' -s -o ${temp_json_file} "http://localhost:8080/api/v1/health")
+  cat ${temp_json_file}
+  if [[ x"${status}" = x200 ]] ; then
+    break
+  else
+    error_count=$((error_count + 1))
+  fi
+  if [[ ${error_count} -ge 60 ]] ; then
+    echo "Fess is not available."
+    cat ${temp_log_file} ./fess-*/logs/*.log
+    exit 1
+  fi
   sleep 1
-  counter=$((counter + 1))
 done
+
+pushd /tmp >/dev/null
+git clone https://github.com/codelibs/fess-testdata.git
+popd >/dev/null
+
+cat ${temp_log_file} ./fess-*/logs/*.log
+curl -s "http://localhost:9201/_cat/indices?v"
+curl -s "http://localhost:8080/"
 
--- src/test/resources/run.sh
+++ src/test/resources/run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-tail -f ./fess-*/logs/fess-crawler.log &
+touch $(ls -d ./fess-*/logs)/fess-crawler.log
+tail -f ./fess-*/logs/*.log &
 
-mvn test -P integrationTests -Dtest.fess.url="http://127.0.0.1:8080" -Dtest.search_engine.url="http://127.0.0.1:9201"
+mvn test -P integrationTests -Dtest.fess.url="http://localhost:8080" -Dtest.search_engine.url="http://localhost:9201"
 ret=$?
 
 if [ $ret != 0 ] ; then
