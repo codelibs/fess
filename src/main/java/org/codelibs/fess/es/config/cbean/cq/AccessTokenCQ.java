76352cf8dce73c4cc9049085237ebd6ad1c98996
Shinsuke Sugaya <shinsuke@apache.org>
2024-02-22 10:53:18 +0900

    Update copyright headers to 2024

--- src/main/java/org/codelibs/fess/es/config/cbean/cq/AccessTokenCQ.java
+++ src/main/java/org/codelibs/fess/es/config/cbean/cq/AccessTokenCQ.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2023 CodeLibs Project and the Others.
+ * Copyright 2012-2024 CodeLibs Project and the Others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
