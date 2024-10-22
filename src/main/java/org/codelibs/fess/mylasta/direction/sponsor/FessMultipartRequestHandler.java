e8ae04073af34be7b3022546dcc23e8fc5c99ae2
Shinsuke Sugaya <shinsuke@apache.org>
2024-10-22 22:46:06 +0900

    replace with jakarta

--- src/main/java/org/codelibs/fess/mylasta/direction/sponsor/FessMultipartRequestHandler.java
+++ src/main/java/org/codelibs/fess/mylasta/direction/sponsor/FessMultipartRequestHandler.java
@@ -61,7 +61,7 @@ public class FessMultipartRequestHandler implements MultipartRequestHandler {
     //                                   Temporary Directory
     //                                   -------------------
     // used as repository for requested parameters
-    protected static final String CONTEXT_TEMPDIR_KEY = "javax.servlet.context.tempdir"; // prior
+    protected static final String CONTEXT_TEMPDIR_KEY = "jakarta.servlet.context.tempdir";
     protected static final String JAVA_IO_TMPDIR_KEY = "java.io.tmpdir"; // secondary
 
     // ===================================================================================
