dc34ad41d5db1c116d32a6b17fa5643b7c09a5cd
Shinsuke Sugaya <shinsuke@apache.org>
2025-05-06 17:46:15 +0900

    Add response.headers support and integrate HttpHeaderSecurityFilter in web.xml

--- src/main/java/org/codelibs/fess/mylasta/direction/sponsor/FessActionAdjustmentProvider.java
+++ src/main/java/org/codelibs/fess/mylasta/direction/sponsor/FessActionAdjustmentProvider.java
@@ -15,21 +15,81 @@
  */
 package org.codelibs.fess.mylasta.direction.sponsor;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.codelibs.core.lang.StringUtil;
+import org.codelibs.core.misc.Pair;
+import org.codelibs.core.stream.StreamUtil;
+import org.codelibs.fess.mylasta.direction.FessConfig;
 import org.codelibs.fess.util.ComponentUtil;
 import org.dbflute.util.DfTypeUtil;
 import org.lastaflute.web.path.ActionAdjustmentProvider;
 import org.lastaflute.web.path.FormMappingOption;
+import org.lastaflute.web.response.ActionResponse;
+import org.lastaflute.web.response.HtmlResponse;
+import org.lastaflute.web.response.JsonResponse;
+import org.lastaflute.web.response.StreamResponse;
+import org.lastaflute.web.response.XmlResponse;
+import org.lastaflute.web.ruts.process.ActionRuntime;
 
 /**
  * @author jflute
  */
 public class FessActionAdjustmentProvider implements ActionAdjustmentProvider {
 
+    private static final Logger logger = LogManager.getLogger(FessActionAdjustmentProvider.class);
+
     // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
     // you can adjust your actions by overriding
     // default methods defined at the interface
     // _/_/_/_/_/_/_/_/_/_/
+
+    protected Map<String, List<Pair<String, String>>> responseHeaderMap = new HashMap<>();
+
+    protected List<Pair<String, String>> defaultResponseHeaders;
+
+    public FessActionAdjustmentProvider(final FessConfig fessConfig) {
+        parseResponseHeaderConfig(fessConfig.getResponseHeaders());
+    }
+
+    private void parseResponseHeaderConfig(final String value) {
+        if (StringUtil.isBlank(value)) {
+            defaultResponseHeaders = Collections.emptyList();
+            return;
+        }
+
+        StreamUtil.split(value, "\n").of(stream -> stream.filter(StringUtil::isNotBlank).forEach(s -> {
+            final String[] values = StringUtils.split(s, "=", 2);
+            List<Pair<String, String>> list = responseHeaderMap.get(values[0]);
+            if (list == null) {
+                list = new ArrayList<>();
+                responseHeaderMap.put(values[0], list);
+            }
+            final String[] keyValue = StringUtils.split(values[1], ":", 2);
+            if (keyValue.length == 2) {
+                list.add(new Pair<>(keyValue[0].trim(), keyValue[1].trim()));
+            } else if (keyValue.length == 1) {
+                list.add(new Pair<>(keyValue[0].trim(), StringUtil.EMPTY));
+            } else {
+                logger.warn("Unexpected value: {}", s);
+            }
+        }));
+
+        final List<Pair<String, String>> headerList = responseHeaderMap.remove("*");
+        if (headerList != null) {
+            defaultResponseHeaders = headerList;
+        } else {
+            defaultResponseHeaders = Collections.emptyList();
+        }
+    }
 
     @Override
     public FormMappingOption adjustFormMapping() {
@@ -56,5 +116,36 @@ public class FessActionAdjustmentProvider implements ActionAdjustmentProvider {
     @Override
     public String toString() {
         return DfTypeUtil.toClassTitle(this) + ":{}";
+    }
+
+    @Override
+    public void adjustActionResponseJustBefore(final ActionRuntime runtime, final ActionResponse response) {
+        final String mimeType;
+        if (response instanceof HtmlResponse) {
+            mimeType = "text/html";
+        } else if (response instanceof JsonResponse) {
+            mimeType = "application/json";
+        } else if (response instanceof XmlResponse) {
+            mimeType = "text/xml";
+        } else if (response instanceof StreamResponse) {
+            mimeType = "application/octet-stream";
+        } else {
+            logger.debug("Unknown response: {}", response);
+            return;
+        }
+        adjustActionResponseHeaders(mimeType, (k, v) -> {
+            if (logger.isDebugEnabled()) {
+                logger.debug("Apply header {}:{} to response of {}", k, v, mimeType);
+            }
+            response.header(k, v);
+        });
+    }
+
+    protected void adjustActionResponseHeaders(final String mimeType, final BiConsumer<String, String> callback) {
+        defaultResponseHeaders.forEach(header -> callback.accept(header.getFirst(), header.getSecond()));
+        final List<Pair<String, String>> headers = responseHeaderMap.get(mimeType);
+        if (headers != null) {
+            headers.forEach(header -> callback.accept(header.getFirst(), header.getSecond()));
+        }
     }
 }
